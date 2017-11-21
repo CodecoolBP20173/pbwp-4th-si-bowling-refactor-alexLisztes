f33719cebeb2a2c6bfc8cea962fd0854188448e9
Alex Lisztes <lisztes.alex@gmail.com>
2017-11-21 16:23:49 +0100

    Worked on complexity, no magic numbers are present

--- bowling.py
+++ bowling.py
@@ -1,18 +1,26 @@
+class Score:
+    spare = '/'
+    strike = 'X'
+    miss = '-'
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', ]
+
+
 def score(game):
     result = 0
     frame = 1
+    last = 0
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
+        if game[i] == Score.spare:
             result += 10 - last
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if game[i] == Score.spare:
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].upper() == Score.strike:
                 result += get_value(game[i + 1])
-                if game[i + 2] == '/':
+                if game[i + 2] == Score.spare:
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
@@ -23,22 +31,20 @@ def score(game):
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i].upper() == Score.strike:
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in Score.nums:
         return int(char)
-    elif char == 'X' or char == 'x':
+    elif char.upper() == Score.strike:
         return 10
-    elif char == '/':
+    elif char == Score.spare:
         return 10
-    elif char == '-':
+    elif char == Score.miss:
         return 0
     else:
         raise ValueError()
