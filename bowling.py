d2fcfdfd549b1309ad277e4ec6b378a6094ccbd0
Alex Lisztes <lisztes.alex@gmail.com>
2017-11-21 17:24:12 +0100

    Extracted methods

--- bowling.py
+++ bowling.py
@@ -2,7 +2,12 @@ class Score:
     spare = '/'
     strike = 'X'
     miss = '-'
-    nums = ['1', '2', '3', '4', '5', '6', '7', '8', ]
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    maxScore = 10
+    missScore = 0
+
+
+TOTAL_NUMBER_OF_FRAMES = 10
 
 
 def score(game):
@@ -11,29 +16,36 @@ def score(game):
     last = 0
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == Score.spare:
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == Score.spare:
-                result += get_value(game[i + 1])
-            elif game[i].upper() == Score.strike:
-                result += get_value(game[i + 1])
-                if game[i + 2] == Score.spare:
-                    result += 10 - get_value(game[i + 1])
-                else:
-                    result += get_value(game[i + 2])
+        result = CalculateScore(game, i, last, result)
+        if frame < TOTAL_NUMBER_OF_FRAMES and get_value(game[i]) == Score.maxScore:
+            result = CalculateScoreIfLastWasTotalled(game, i, result)
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True
+        in_first_half = not in_first_half
         if game[i].upper() == Score.strike:
             in_first_half = True
             frame += 1
+    return result
+
+
+def CalculateScore(game, i, last, result):
+    if game[i] == Score.spare:
+        result += 10 - last
+    else:
+        result += get_value(game[i])
+    return result
+
+
+def CalculateScoreIfLastWasTotalled(game, i, result):
+    if game[i] == Score.spare:
+        result += get_value(game[i + 1])
+    elif game[i].upper() == Score.strike:
+        result += get_value(game[i + 1])
+        if game[i + 2] == Score.spare:
+            result += Score.maxScore - get_value(game[i + 1])
+        else:
+            result += get_value(game[i + 2])
     return result
 
 
@@ -41,10 +53,10 @@ def get_value(char):
     if char in Score.nums:
         return int(char)
     elif char.upper() == Score.strike:
-        return 10
+        return Score.maxScore
     elif char == Score.spare:
-        return 10
+        return Score.maxScore
     elif char == Score.miss:
-        return 0
+        return Score.missScore
     else:
         raise ValueError()
